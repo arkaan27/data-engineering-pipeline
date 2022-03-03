4f02d57124f57811ab25063a3a3a7e15927ea4d4
arkaan27 <arkaankanunga@gmail.com>
2022-03-03 21:22:47 +0000

    Setting up basic main function for running the pipeline

---
+++ main.py
@@ -0,0 +1,20 @@
+"""
+Main function to run the script of the pipeline
+
+Author: Arkaan Quanunga
+Date: 03/03/2022
+
+"""
+
+
+def basic_function(paratemeter1):
+    """
+    Setting up the project: Prints the parameter
+    :param paratemeter1: [str]
+    :return:
+    """
+    print(paratemeter1)
+
+
+if __name__ == "__main__":
+    basic_function("hello")
