734657414452c5f3e53777df2bb004170d91ec0f
arkaan27 <arkaankanunga@gmail.com>
2022-03-05 00:25:28 +0000

    Created pipeline with steps using Hydra, config.yaml; Added data_upload step to the pipeline

--- main.py
+++ main.py
@@ -5,18 +5,47 @@ Author: Arkaan Quanunga
 Date: 03/03/2022
 
 """
-import os.path
+import json
+
+import mlflow
+import tempfile
 import os
+import wandb
+import hydra
+from omegaconf import DictConfig
+
+_steps = [
+    "data_upload",
+    "data_processing",
+    "data_check",
+    "database_write"
+]
 
 
-def basic_function(parameter1):
-    """
-    Setting up the project: Prints the parameter
-    :param paratemeter1: [str]
-    :return:
-    """
-    print(parameter1)
+@hydra.main(config_name='config')
+def go(config: DictConfig):
+
+    # Setup the wandb experiment. All runs will be grouped under this name
+    os.environ["WANDB_PROJECT"] = config["main"]["project_name"]
+    os.environ["WANDB_RUN_GROUP"] = config["main"]["experiment_name"]
+
+    # Steps to execute
+    steps_par = config['main']['steps']
+    active_steps = steps_par.split(",") if steps_par != "all" else _steps
+
+    if "data_upload" in active_steps:
+        # Upload file and load in W&B
+        _ = mlflow.run(
+            os.path.join(hydra.utils.get_original_cwd(), "src", "components","data_upload_WandB"),
+            "main",
+            parameters={
+                "bucket_path": config['data_upload']['bucket_path'],
+                "output_artifact": "raw_data",
+                "output_type": "data_upload",
+                "output_description": "Artifact for storage of data on Weights & Biases",
+            },
+        )
 
 
 if __name__ == "__main__":
-    basic_function("hello")
+    go()
