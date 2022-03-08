427e38d8871c6faf3b6d24b018a254691980f963
arkaan27 <arkaankanunga@gmail.com>
2022-03-08 16:18:34 +0000

    Deleted testing.py as it's irrelevant file

--- testing.py
+++
@@ -1,9 +0,0 @@
-import socket
-from requests import get
-if __name__ == "__main__":
-
-    ip = get('https://api.ipify.org').content.decode('utf8')
-    print('My public IP address is: {}'.format(ip))
-
-    hostname= ip + ":27017"
-    print(hostname)
