0f2f3216637c5658285928e1c3add5242f2360b6
arkaan27 <arkaankanunga@gmail.com>
2022-03-14 21:02:41 +0000

    Fixing the import errors

--- src/components/setup.py
+++ src/components/setup.py
@@ -1,12 +1,11 @@
 from setuptools import setup
 
-
 setup(
-    name="data_upload_S3",
+    name="Utils",
     version=0.1,
-    description="Utilities for uploading data to S3 bucket",
+    description="Utilities for making the program function",
     zip_safe=False,  # avoid eggs, which make the handling of package data cumbersome
-    packages=["data_upload_S3"],
+    packages=["data_upload_S3", "Utils"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "Development Status :: 4 - Beta",
@@ -15,22 +14,5 @@ setup(
         "mlflow",
         "wandb",
         "boto3"
-    ]
-)
-
-setup(
-    name="Utils",
-    version=0.1,
-    description="Utilities for the programme to run",
-    zip_safe=False,  # avoid eggs, which make the handling of package data cumbersome
-    packages=["Utils"],
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "Development Status :: 4 - Beta",
-    ],
-    install_requires=[
-        "mlflow",
-        "wandb",
-        "pymongo"
     ]
 )
